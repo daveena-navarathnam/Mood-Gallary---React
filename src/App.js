a8a68a4ce9ca5dbea3f53447cad31fe86a51298b
daveena-navarathnam <daveena.navarathnam@gmail.com>
2026-03-29 16:10:27 +0200

    fix alt attribute

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 function Gallery(props) {
     return (
         <div className="card" >
-            <img src={props.url} alt="image" />
+            <img src={props.url} alt={props.title}/>
             <h1>{props.title}</h1>
             <p>{props.description}</p>
         </div>
